ab245cceec287f4eb66718fd81b7a742114e741d
desgramado73 <alisson.henriquedsn@gmail.com>
2025-01-07 23:17:21 -0300

    Delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: seller findById ====");
@@ -38,6 +40,23 @@ public class Program {
 		
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n === TEST 5: seller Update ====");
+		
+		seller = sellerDao.findById(1);
+		
+		seller.setName("Martha Weine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
+		System.out.println("\n === TEST 6: seller Delete ====");
+		System.out.println("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed");
+		
+		sc.close();
 	}
 
 }
